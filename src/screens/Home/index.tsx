83c90da839b639b59296d0e055141a84e9ba3035
mars1dev <marcelombr28@gmail.com>
2022-11-08 18:08:51 -0300

    iniciando o app1

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,38 @@
+import { Text, View, TextInput, TouchableOpacity } from "react-native"
+import { styles } from "./styles"
+
+
+
+export default function Home(){
+
+  function handleParticipantAdd (){
+    console.log("Voce clicou no botao de adicionar!");
+  }
+   return(
+    <View style={styles.container}>
+      <Text style={styles.eventName} >
+        Nome do evento
+        </Text>
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022,
+        </Text>
+
+        {/* view criada para mudar o flex direction */}
+    <View style={styles.form}>
+        <TextInput
+         style={styles.input} 
+         placeholder="Nome do participante"
+         placeholderTextColor={'#6B6B6B'}
+         />
+      
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}> 
+          +
+        </Text>
+      </TouchableOpacity>
+      </View>
+    </View>
+
+
+   )
+}
